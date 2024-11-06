c4b45613991717f4a0a2db5409bc1e78c332ceaa
Bben95 <benakabp@gmail.com>
2024-11-06 17:08:13 +0530

    added Nav bar and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+   <footer className="flexStart footer">
+    <div className="flex flex-col gap-12 w-full">
+        <div>
+            
+        </div>
+
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
